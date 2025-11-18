2094a87caabab9fae1c49b87e70bebaefe980bbe
rachan310 <rachanr310@gmail.com>
2025-11-18 15:47:14 +0530

    Create user details.c

---
+++ user details.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int rachan()
+{
+    char name,department;
+    int age;
+    printf("enter a name\n");
+    scanf("%s",&name);
+    printf("enter a age\n");
+    scanf("%d",&age);
+    printf("enter a department\n");
+    scanf("%s",&department);
+}
+int main() {      
+    rachan();
+}
